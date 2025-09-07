668215f8d66ac6b4212abadd7ade8ff90c89df3b
N1K0232 <ns.nicolasilvestri@libero.it>
2025-09-07 11:13:14 +0200

    Refactoring

--- src/AddressBook.Database/dbo/Tables/Cities.sql
+++ src/AddressBook.Database/dbo/Tables/Cities.sql
@@ -1,11 +1,22 @@
 ﻿CREATE TABLE [dbo].[Cities] (
-    [Id]             UNIQUEIDENTIFIER CONSTRAINT [DF_Cities_Id] DEFAULT (newsequentialid()) NOT NULL,
+    [Id]             UNIQUEIDENTIFIER NOT NULL,
     [Name]           NVARCHAR (100)   NOT NULL,
-    [CreatedAt]      DATETIME         CONSTRAINT [DF_Cities_CreatedAt] DEFAULT (getutcdate()) NOT NULL,
-    [LastModifiedAt] DATETIME         NULL,
-    CONSTRAINT [PK_Cities] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [CreatedAt]      DATETIME         NOT NULL,
+    [LastModifiedAt] DATETIME         NULL
 );
 
 GO
+ALTER TABLE [dbo].[Cities]
+ADD CONSTRAINT [PK_Cities] PRIMARY KEY ([Id] ASC);
+
+GO
+ALTER TABLE [dbo].[Cities]
+ADD CONSTRAINT [DF_Cities_Id] DEFAULT (NEWSEQUENTIALID()) FOR [Id];
+
+GO
+ALTER TABLE [dbo].[Cities]
+ADD CONSTRAINT [DF_Cities_CreatedAt] DEFAULT (GETUTCDATE()) FOR [CreatedAt];
+
+GO
 CREATE UNIQUE NONCLUSTERED INDEX [IX_Cities_Name]
-    ON [dbo].[Cities]([Id] ASC);
+    ON [dbo].[Cities]([Name] ASC);
--- src/AddressBook.Database/dbo/Tables/People.sql
+++ src/AddressBook.Database/dbo/Tables/People.sql
@@ -1,11 +1,25 @@
 ﻿CREATE TABLE [dbo].[People] (
-    [Id]             UNIQUEIDENTIFIER CONSTRAINT [DF_People_Id] DEFAULT (newsequentialid()) NOT NULL,
+    [Id]             UNIQUEIDENTIFIER NOT NULL,
     [CityId]         UNIQUEIDENTIFIER NOT NULL,
     [FirstName]      NVARCHAR (255)   NOT NULL,
     [LastName]       NVARCHAR (255)   NOT NULL,
     [BirthDate]      DATE             NOT NULL,
-    [CreatedAt]      DATETIME         CONSTRAINT [DF_People_CreatedAt] DEFAULT (getutcdate()) NOT NULL,
+    [CreatedAt]      DATETIME         NOT NULL,
     [LastModifiedAt] DATETIME         NULL,
-    CONSTRAINT [PK_People] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_People_Cities] FOREIGN KEY ([CityId]) REFERENCES [dbo].[Cities] ([Id])
 );
+
+GO
+ALTER TABLE [dbo].[People]
+ADD CONSTRAINT [PK_People] PRIMARY KEY ([Id] ASC);
+
+GO
+ALTER TABLE [dbo].[People]
+ADD CONSTRAINT [FK_People_Cities] FOREIGN KEY ([CityId]) REFERENCES [dbo].[Cities]([Id]);
+
+GO
+ALTER TABLE [dbo].[People]
+ADD CONSTRAINT [DF_People_Id] DEFAULT (NEWSEQUENTIALID()) FOR [Id];
+
+GO
+ALTER TABLE [dbo].[People]
+ADD CONSTRAINT [DF_People_CreatedAt] DEFAULT (GETUTCDATE()) FOR [CreatedAt];
